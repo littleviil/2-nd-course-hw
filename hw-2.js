5fa5e1d9b01b9f4f356b57e59ed22e76ac266cca
littleviil <netlena30@gmail.com>
2024-02-12 19:31:57 +0300

    Task 8

--- hw-2.js
+++ hw-2.js
@@ -34,11 +34,16 @@
 
 
 //7
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num++;
-num--;
-alert(num);
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num++;
+// num--;
+// alert(num);
+
+
+//8
+let age = Number(prompt(`Сколько вам лет?`));
+alert(age);
