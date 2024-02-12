8c0a948c17ebc0587b0f76a592cad49acf1a7a1d
littleviil <netlena30@gmail.com>
2024-02-12 18:51:05 +0300

    the first 6 tasks in js

---
+++ hw-2.js
@@ -0,0 +1,36 @@
+//1
+// let num = 10;
+// const up = 20;
+// alert(num);
+// alert(`${num} + ${up} = ${num + up}`);
+
+
+//2
+// const yearReleaseIphone = 2007;
+// alert(`Год выпуска первого Iphone: ${yearReleaseIphone}`);
+
+
+//3
+// const nameCreatorJavaScript = "Брендан Эйх";
+// alert(`Имя создателя JavaScript: ${nameCreatorJavaScript}`);
+
+
+//4
+// let num_1 = 10;
+// let num_2 = 2;
+// alert(`Числа: ${num_1}; ${num_2}.\nСумма: ${num_1 + num_2}\nРазность: ${num_1 - num_2}\nПроизведение: ${num_1 * num_2}\nЧастное: ${num_1 / num_2}\n `);
+
+
+//5
+// let num = 2;
+// num **= 5;
+// alert(num);
+
+
+//6
+// let a = 9;
+// let b = 2;
+// alert(`Остаток от деления: ${a % b}`);
+
+
+//7
