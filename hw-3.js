0b4eb506e11f48540a9a245f6973c52935f27d51
littleviil <netlena30@gmail.com>
2024-02-19 15:38:03 +0300

    2 task

--- hw-3.js
+++ hw-3.js
@@ -1,12 +1,18 @@
 //1
-let password = 'Skypro';
-let password_user = prompt(`Введите пароль: `);
+// let password = 'Skypro';
+// let password_user = prompt(`Введите пароль: `);
 // if (password === password_user) {
 //     alert('Пароль введен верно');
 // } else {
 //     alert('Пароль введен неверно');
 // }
-alert(password === password_user ? "Пароль введен верно" : "Пароль введен неверно")
+// alert(password === password_user ? "Пароль введен верно" : "Пароль введен неверно");
 
 
 //2
+let c = 2;
+if (c > 0 && c < 10) {
+    console.log('Верно');
+} else {
+    console.log('Неврно');
+}
