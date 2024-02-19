7e742dca7dc9fa485b8a2aa319f83b2bf85ee6ba
littleviil <netlena30@gmail.com>
2024-02-19 17:04:31 +0300

    5 task

--- hw-3.js
+++ hw-3.js
@@ -25,6 +25,38 @@
 
 
 //4
-let a = '2';
-let b = '3';
-alert(Number(a) + Number(b));
+// let a = '2';
+// let b = '3';
+// alert(Number(a) + Number(b));
+
+
+//5
+var monthNumber = prompt("Введите номер месяца");
+
+switch (monthNumber) {
+    case '1':
+    case '2':
+    case '3':
+        console.log('Зима');
+        break;
+    case '4':
+    case '5':
+    case '6':
+        console.log('Весна');
+        break;
+    case '7':
+    case '8':
+    case '9':
+        console.log('Лето');
+        break;
+    case '10':
+    case '11':
+    case '12':
+        console.log('Осень');
+        break;
+    default:
+        console.log('Такого месяца не существует');
+}
+
+
+//6
