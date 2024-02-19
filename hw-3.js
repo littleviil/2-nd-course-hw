0786a5bc2363326f7a5e0c184402eb072c44ad2a
littleviil <netlena30@gmail.com>
2024-02-19 17:35:35 +0300

    add footer

--- hw-3.js
+++ hw-3.js
@@ -31,32 +31,32 @@
 
 
 //5
-var monthNumber = prompt("Введите номер месяца");
+// var monthNumber = prompt("Введите номер месяца");
 
-switch (monthNumber) {
-    case '1':
-    case '2':
-    case '3':
-        console.log('Зима');
-        break;
-    case '4':
-    case '5':
-    case '6':
-        console.log('Весна');
-        break;
-    case '7':
-    case '8':
-    case '9':
-        console.log('Лето');
-        break;
-    case '10':
-    case '11':
-    case '12':
-        console.log('Осень');
-        break;
-    default:
-        console.log('Такого месяца не существует');
-}
+// switch (monthNumber) {
+//     case '1':
+//     case '2':
+//     case '3':
+//         console.log('Зима');
+//         break;
+//     case '4':
+//     case '5':
+//     case '6':
+//         console.log('Весна');
+//         break;
+//     case '7':
+//     case '8':
+//     case '9':
+//         console.log('Лето');
+//         break;
+//     case '10':
+//     case '11':
+//     case '12':
+//         console.log('Осень');
+//         break;
+//     default:
+//         console.log('Такого месяца не существует');
+// }
 
 
 //6
