16aa8e82957e1744cb3b29c000acae5aff79e06c
littleviil <netlena30@gmail.com>
2024-02-19 15:31:40 +0300

    1 task

--- hw-2.js
+++ hw-2.js
@@ -1,21 +1,21 @@
 //1
 // let num = 10;
-// const up = 20;
 // alert(num);
-// alert(`${num} + ${up} = ${num + up}`);
+// num = 20;
+// alert(num);
 
 
-//2
+// //2
 // const yearReleaseIphone = 2007;
 // alert(`Год выпуска первого Iphone: ${yearReleaseIphone}`);
 
 
-//3
+// //3
 // const nameCreatorJavaScript = "Брендан Эйх";
 // alert(`Имя создателя JavaScript: ${nameCreatorJavaScript}`);
 
 
-//4
+// //4
 // let num_1 = 10;
 // let num_2 = 2;
 // alert(`Числа: ${num_1}; ${num_2}.\nСумма: ${num_1 + num_2}\nРазность: ${num_1 - num_2}\nПроизведение: ${num_1 * num_2}\nЧастное: ${num_1 / num_2}\n `);
@@ -27,7 +27,7 @@
 // alert(num);
 
 
-//6
+// //6
 // let a = 9;
 // let b = 2;
 // alert(`Остаток от деления: ${a % b}`);
@@ -44,32 +44,32 @@
 // alert(num);
 
 
-//8
-// let age = Number(prompt(`Сколько вам лет?`));
-// alert(age);
+// //8
+let age = Number(prompt("Сколько вам лет?"));
+alert(age);
 
 
-// //9
+// // //9
 // let user = {
 //     name: 'Jimmy',
 //     age: 20,
 //     isAdmin: true
 // };
 
-// //9.1
+// // //9.1
 // user['city of residence'] = "Helsinki";
 
-// //9.2
+// // //9.2
 // user.age = 35;
 
-// //9.3
+// // //9.3
 // delete user['city of residence'];
 
-// //9.4
+// // //9.4
 // let info = prompt("Какую информацию хотите узнать о пользователе?");
 // alert(user[info]);
 
 
-//10
-let username = String(prompt(`Введите ваше имя: `));
-alert(`Привет, ${username}`);
+// //10
+// let username = prompt(`Введите ваше имя: `);
+// alert(`Привет, ${username}`);
---
+++ hw-3.js
@@ -0,0 +1,12 @@
+//1
+let password = 'Skypro';
+let password_user = prompt(`Введите пароль: `);
+// if (password === password_user) {
+//     alert('Пароль введен верно');
+// } else {
+//     alert('Пароль введен неверно');
+// }
+alert(password === password_user ? "Пароль введен верно" : "Пароль введен неверно")
+
+
+//2
