913c14c7ec9fc199e6dcde1d00fb339dd46675cc
littleviil <netlena30@gmail.com>
2024-02-21 23:19:08 +0300

    Implementation of the 6th task according to the conditions

--- hw-4.js
+++ hw-4.js
@@ -41,21 +41,26 @@
 
 //6
 //Расчет на 2024 год
-let day = 31;
-let month = 1;
-for (let i = 5; i <= day; i += 7) {
-    if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
-        day = 31;
-    } else if (month === 4 || month === 6 || month === 9 || month === 11) {
-        day = 30;
-    } else day = 29;
-    if ((i + 7) > day && month < 12) {
-        console.log(`Сегодня ${i} число(пятница) ${month} месяц, необходмо подготовить отчет`);
-        i = i + 7 - day;
-        month++;
-    }
-    console.log(`Сегодня ${i} число(пятница) ${month} месяц, необходмо подготовить отчет`);
-    if (month === 12 && i + 7 > day) {
-        break;
-    }
+// let day = 31;
+// let month = 1;
+// for (let i = 5; i <= day; i += 7) {
+//     if (month === 1 || month === 3 || month === 5 || month === 7 || month === 8 || month === 10 || month === 12) {
+//         day = 31;
+//     } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+//         day = 30;
+//     } else day = 29;
+//     if ((i + 7) > day && month < 12) {
+//         console.log(`Сегодня ${i} число(пятница) ${month} месяц, необходмо подготовить отчет`);
+//         i = i + 7 - day;
+//         month++;
+//     }
+//     console.log(`Сегодня ${i} число(пятница) ${month} месяц, необходмо подготовить отчет`);
+//     if (month === 12 && i + 7 > day) {
+//         break;
+//     }
+// }
+
+//Реализация по условиям дз
+for (let i = 5; i <= 31; i += 7) {
+    alert(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`);
 }
