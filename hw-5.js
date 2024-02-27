6929b621a60f37d06071fb6a03067548fc1cc878
littleviil <netlena30@gmail.com>
2024-02-27 11:11:04 +0300

    2 task

--- hw-5.js
+++ hw-5.js
@@ -1,13 +1,19 @@
 //1
-function comparison(a, b) {
-    let min;
-    if (a > b) {
-        min = b;
-    } else if (a < b) {
-        min = a;
-    } else min = a;
-    return min;
+// function comparison(a, b) {
+//     let min;
+//     if (a > b) {
+//         min = b;
+//     } else if (a < b) {
+//         min = a;
+//     } else min = a;
+//     return min;
+// }
+// console.log(comparison(5, 1));
+// console.log(comparison(100, 101));
+// console.log(comparison(3, 3));
+
+//2
+function parity(a) {
+    a % 2 == 0 ? alert("Число четное") : alert("Число нечетное");
 }
-console.log(comparison(5, 1));
-console.log(comparison(100, 101));
-console.log(comparison(3, 3));
+parity(prompt("Введите число: "));
