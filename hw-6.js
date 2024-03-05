cdde915ea73fd904aa37a4562185a90f958efc06
littleviil <netlena30@gmail.com>
2024-03-05 11:31:06 +0300

    6 task

--- hw-6.js
+++ hw-6.js
@@ -32,8 +32,22 @@
 // }
 
 //5
-let array_5 = [1, 1, 1];
-for (let i = 0; i < 3; i++) {
-    array_5.push(2);
+// let array_5 = [1, 1, 1];
+// for (let i = 0; i < 3; i++) {
+//     array_5.push(2);
+// }
+// console.log(array_5);
+
+//6
+let array_6 = [9, 8, 7, 'a', 6, 5];
+const value = array_6.filter(array_val => array_val != 'a');
+console.log(value);
+let step = 0;
+for (let i = 0; i < value.length; i++) {
+    for (let j = 0; j < value.length - i; j++) {
+        if (value[j] > value[j + 1]) {
+            [value[j], value[j + 1]] = [value[j + 1], value[j]]; // Меняем значения переменных
+        }
+    }
 }
-console.log(array_5);
+console.log(value);
