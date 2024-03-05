4a5f3fa804890419d0590d0eabf843dada200385
littleviil <netlena30@gmail.com>
2024-03-05 10:08:25 +0300

    2 task

--- hw-6.js
+++ hw-6.js
@@ -1,8 +1,12 @@
 //1
-let array = [1, 5, 4, 10, 0, 3];
-for (let i = 0; i < array.length; i++) {
-    console.log(array[i]);
-    if (array[i] === 10) {
-        break;
-    }
-}
+// let array_1 = [1, 5, 4, 10, 0, 3];
+// for (let i = 0; i < array_1.length; i++) {
+//     console.log(array_1[i]);
+//     if (array_1[i] === 10) {
+//         break;
+//     }
+// }
+
+//2
+let array_2 = [1, 5, 4, 10, 0, 3];
+console.log(array_2.indexOf(4));
