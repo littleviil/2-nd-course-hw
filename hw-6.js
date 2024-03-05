9f04de90bcada171cc6c3df8f17265494b726ff2
littleviil <netlena30@gmail.com>
2024-03-06 02:16:45 +0300

    7 task

--- hw-6.js
+++ hw-6.js
@@ -39,15 +39,36 @@
 // console.log(array_5);
 
 //6
-let array_6 = [9, 8, 7, 'a', 6, 5];
-const value = array_6.filter(array_val => array_val != 'a');
-console.log(value);
-let step = 0;
-for (let i = 0; i < value.length; i++) {
-    for (let j = 0; j < value.length - i; j++) {
-        if (value[j] > value[j + 1]) {
-            [value[j], value[j + 1]] = [value[j + 1], value[j]]; // Меняем значения переменных
-        }
+// let array_6 = [9, 8, 7, 'a', 6, 5];
+// const value = array_6.filter(array_val => array_val != 'a');
+// console.log(value);
+// let step = 0;
+// for (let i = 0; i < value.length; i++) {
+//     for (let j = 0; j < value.length - i; j++) {
+//         if (value[j] > value[j + 1]) {
+//             [value[j], value[j + 1]] = [value[j + 1], value[j]]; // Меняем значения переменных
+//         }
+//     }
+// }
+// console.log(value);
+
+//7
+let array_7 = [9, 8, 7, 6, 5];
+let num = Number(prompt("Введите возможное число массива"));
+
+//Первая задумка
+let done = 0;
+for (let i = 0; i < array_7.length; i++) {
+    if (array_7[i] == num) {
+        alert("Угадал");
+        done = 1;
     }
 }
-console.log(value);
+if (done === 0) {
+    alert("Не угадал");
+}
+
+//Подсказка из дз
+if (array_7.includes(num) === true) {
+    alert("Угадал");
+} else { alert("Не угадал"); }
