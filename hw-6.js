2b72f4b2e39598f2f06115c0b2dd8d81f1d2f78f
littleviil <netlena30@gmail.com>
2024-03-05 10:57:49 +0300

    4 task

--- hw-6.js
+++ hw-6.js
@@ -12,5 +12,21 @@
 // console.log(array_2.indexOf(4));
 
 //3
-let array_3 = [1, 3, 5, 10, 20];
-console.log(array_3.join(' '));
+// let array_3 = [1, 3, 5, 10, 20];
+// console.log(array_3.join(' '));
+
+//4
+let array_4 = [
+    [1, 1, 1],
+    [1, 1, 1],
+    [1, 1, 1]
+]
+let k = 1;
+for (let i = 0; i < array_4.length; i++) {
+    console.log(`Блок ${k}: `);
+    for (let j = 0; j < 3; j++) {
+        console.log(`${array_4[j]}`);
+    }
+    console.log(`\n`);
+    k++;
+}
