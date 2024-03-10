4cd1807634065999eed79dee10c82bdc28a340a2
littleviil <netlena30@gmail.com>
2024-03-10 21:09:50 +0300

    changes 9 task

--- hw-6.js
+++ hw-6.js
@@ -59,24 +59,17 @@
 // } else { alert("Не угадал"); }
 
 //8
-let str = 'abcdef';
-let strRev = str.split('').reverse().join('');
-console.log(strRev);
+// let str = 'abcdef';
+// let strRev = str.split('').reverse().join('');
+// console.log(strRev);
 
 //9
-// let array_9 = [
-//     [1, 2, 3],
-//     [4, 5, 6]
-// ];
-// let i = 0;
-// for (let j = 3; j < 6; j++) {
-//     array_9[i][j] = array_9[i + 1][j - 3];
-//     //console.log(array_9[i][j]);
-// }
-// array_9.pop();
-// for (let j = 0; j < array_9.length; j++) {
-//     console.log(array_9[j]);
-// }
+let array_9 = [
+    [1, 2, 3],
+    [4, 5, 6]
+];
+array_9.flat(Infinity);
+console.log(`${array_9}`);
 
 //10
 // const generateArray = (length, max) => (
