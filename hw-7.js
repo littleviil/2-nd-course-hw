633baefb0744a425fd6b812c3d6b0df92341f716
littleviil <netlena30@gmail.com>
2024-03-11 21:27:26 +0300

    2 task

--- hw-7.js
+++ hw-7.js
@@ -1,3 +1,10 @@
 //1
-const str_1 = 'js';
-console.log(str_1.toUpperCase());
+// const str_1 = 'js';
+// console.log(str_1.toUpperCase());
+
+//2
+function search(array, str) {
+    return array.filter(string => string.toLowerCase(str).startsWith(str));
+}
+console.log(search(['Яблоко', 'Ябеда', 'Якорь'], 'яб'));
+console.log(search(['Сено', 'восток', 'Верба'], 'к'));
