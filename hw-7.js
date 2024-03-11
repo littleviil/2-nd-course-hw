6739601d3d40454ecd844cf2de41be85b6ac1dd2
littleviil <netlena30@gmail.com>
2024-03-11 21:33:32 +0300

    3 task

--- hw-7.js
+++ hw-7.js
@@ -3,8 +3,12 @@
 // console.log(str_1.toUpperCase());
 
 //2
-function search(array, str) {
-    return array.filter(string => string.toLowerCase(str).startsWith(str));
-}
-console.log(search(['Яблоко', 'Ябеда', 'Якорь'], 'яб'));
-console.log(search(['Сено', 'восток', 'Верба'], 'к'));
+// function search(array, str) {
+//     return array.filter(string => string.toLowerCase(str).startsWith(str));
+// }
+// console.log(search(['Яблоко', 'Ябеда', 'Якорь'], 'яб'));
+// console.log(search(['Сено', 'восток', 'Верба'], 'к'));
+
+//3
+let num = 32.58884;
+console.log(Math.floor(num), Math.ceil(num), Math.round(num));
