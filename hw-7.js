9b3a921b8bad9b0efb1dc602c189007d70741bc4
littleviil <netlena30@gmail.com>
2024-03-11 21:46:21 +0300

    5 task

--- hw-7.js
+++ hw-7.js
@@ -10,9 +10,16 @@
 // console.log(search(['Сено', 'восток', 'Верба'], 'к'));
 
 //3
-// let num = 32.58884;
-// console.log(Math.floor(num), Math.ceil(num), Math.round(num));
+// let num_3 = 32.58884;
+// console.log(Math.floor(num_3), Math.ceil(num_3), Math.round(num_3));
 
 //4
-let arr_4 = [52, 53, 49, 77, 21, 32];
-console.log(Math.min(...arr_4), Math.max(...arr_4));
+// let arr_4 = [52, 53, 49, 77, 21, 32];
+// console.log(Math.min(...arr_4), Math.max(...arr_4));
+
+//5
+function random() {
+    let num_5 = (Math.random() * 10) + 1;
+    console.log(Math.floor(num_5));
+}
+random();
