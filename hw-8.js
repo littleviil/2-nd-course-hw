88dbfb79c8f1fb818cb810c5f4c9579dfa261e13
littleviil <netlena30@gmail.com>
2024-03-12 13:51:11 +0300

    start

---
+++ hw-8.js
@@ -0,0 +1,9 @@
+//1
+const people = [
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
+];
+
+console.log(people.sort(human => Math.min(human)));
